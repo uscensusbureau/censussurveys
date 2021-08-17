9cb9d525a8b1a80c8cf1e047c160502894bdea1d
Logan Powell <loganp@tepper.cmu.edu>
2021-08-17 16:47:21 -0400

    removes arbitrary save button in modal

--- assets/script.js
+++ assets/script.js
@@ -28,7 +28,6 @@ const modal = (title, content, id) => `
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
         </div>
       </div>
     </div>
